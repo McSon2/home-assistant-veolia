e8e228bfaa573c038ff3850b09ef63237d6d5da4
McSon2 <maximesaltet@gmail.com>
2024-05-29 01:35:12 +0200

    modified:   run.sh 	modified:   veolia/veolia.py

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
-
 #!/usr/bin/env bash
+export USERNAME="votre_email"
+export PASSWORD="votre_mot_de_passe"
+
+# Afficher les variables d'environnement pour le débogage
+echo "USERNAME: $USERNAME"
+echo "PASSWORD: $PASSWORD"
+
 python3 /app/veolia/veolia.py
