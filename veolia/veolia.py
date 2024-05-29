416725267e92a8faeae56fcf86c6cc93911072eb
McSon2 <maximesaltet@gmail.com>
2024-05-29 02:32:09 +0200

    modified:   veolia/veolia.py

--- veolia/veolia.py
+++ veolia/veolia.py
@@ -9,24 +9,29 @@ password = os.getenv("PASSWORD")
 print(f"USERNAME: {username}")
 print(f"PASSWORD: {password}")
 
-client = VeoliaClient(email=username, password=password)
-try:
-    if client.login():
-        print("Connexion réussie")
-        try:
-            daily_data = client.update(month=False)
-            print("Données de consommation journalière :")
-            print(json.dumps({"history": daily_data}, default=str))
-        except Exception as e:
-            print(f"Erreur lors de la récupération des données journalières: {e}")
+client = VeoliaClient(email="maximesaltet@gmail.com", password="Happy.Fifi@1994")
 
-        try:
-            monthly_data = client.update(month=True)
-            print("Données de consommation mensuelle :")
-            print(json.dumps({"history": monthly_data}, default=str))
-        except Exception as e:
-            print(f"Erreur lors de la récupération des données mensuelles: {e}")
-    else:
-        print("Erreur de connexion")
+# Se connecter
+try:
+    client.login()
+    print("Connexion réussie")
 except Exception as e:
     print(f"Erreur de connexion: {e}")
+
+# Récupérer les données de consommation journalière
+try:
+    data_daily = client.update(month=False)
+    print("Données de consommation journalière :")
+    #print(data_daily)
+    print(json.dumps({"history": data_daily}, default=str))
+except Exception as e:
+    print(f"Erreur lors de la récupération des données journalières: {e}")
+
+# Récupérer les données de consommation mensuelle
+try:
+    data_monthly = client.update(month=True)
+    print("Données de consommation mensuelle :")
+    #print(data_monthly)
+    print(json.dumps({"history": data_monthly}, default=str))
+except Exception as e:
+    print(f"Erreur lors de la récupération des données mensuelles: {e}")
